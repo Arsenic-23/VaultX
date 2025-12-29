ab221478e58c566e4bb36df2a65cc28f2c214db8
utkarsh <156888342+Arsenic-23@users.noreply.github.com>
2025-12-29 17:34:07 +0530

    Add NavbarWrapper component to conditionally render Navbar

---
+++ vaultx-frontend /components/NavbarWrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+
+export default function NavbarWrapper() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/dashboard")) return null;
+  return <Navbar />;
+}
